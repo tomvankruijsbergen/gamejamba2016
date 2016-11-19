27fdc9a7c53869656e7a9c9d1b8b9cf20b56d84b
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-11-19 12:31:03 +0100

    Removed example. Added container example

--- GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/Container.cs
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/Container.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Container : MonoSingleton<Container> {
 
-		
-	public delegate void _OnTest(bool b);
-	public _OnTest OnTest;
+	// All messages between objects should go through this class.
+	// An object always calls this class, and this class then sends events.
+	// Example flow:
+	// - Player.cs:  		sends Container.instance.playerMoved(this.transform.position);
+	// - Container.cs: 		sends this.AudioChanged("something");
+	// - AudioManager.cs:	does things like this.UpdateBackgroundSoundForPlayerPosition(newPosition);
 
 	private AudioManager audioManager;
 
-	void Start () {
-		this.audioManager = new AudioManager();
-		
-		this.OnTest += (bool b) => {};
-		this.OnTest(true);
+	// These are all the events that this class sends.
+	public delegate void _AudioChanged(string test);
+	public event _AudioChanged AudioChanged;
+
+	public override void Init () {
+		this.audioManager = new AudioManager();	
 	}
 
-
-	void Update () {
-		
+	// These functions are called by objects.
+	
+	public void playerMoved(Vector3 newPosition) {
+		// Tell the audio manager that the background music should change.
+		this.AudioChanged("the audio has changed to something");
 	}
 }
