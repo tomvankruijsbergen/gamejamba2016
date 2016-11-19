45626be03532a391a209fbd15b235177623cf29d
Freek <freektimmers@gmail.com>
2016-11-19 12:00:53 +0100

    Started angrybirds movement

--- GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/Container.cs
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/Container.cs
@@ -3,13 +3,36 @@ using System.Collections;
 
 public class Container : MonoSingleton<Container> {
 
+		Vector3 mMouseDownPos;
+	Vector3 mMouseUpPos;
+	public float speed = .1f;
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
+	}
 	
+	void OnMouseDown() 
+	{
+		mMouseDownPos = Input.mousePosition;
+		Debug.Log( "the mouse down pos is " + mMouseDownPos.y.ToString() );
+		mMouseDownPos = Input.mousePosition;
+		Debug.Log( "the mouse down pos is " + mMouseDownPos.z.ToString() );
+		mMouseDownPos.z = 0;
+	}
+	
+	void OnMouseUp() 
+	{
+		mMouseUpPos = Input.mousePosition;
+		mMouseUpPos = Input.mousePosition;
+		mMouseUpPos.z = 0;
+		var direction = mMouseDownPos - mMouseUpPos;
+		direction.Normalize();
+		GetComponent<Rigidbody>().AddForce (direction * speed);
+		Debug.Log( "the mouse up pos is " + mMouseUpPos.ToString() );
 	}
 }
