5d41edae6e44698b00d4bc3c95ed90e2d44f9b8e
Freek <freektimmers@gmail.com>
2016-11-19 13:02:55 +0100

    Hakkemdoordemidden Progress

--- GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/DragCatapultMovement.cs
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/DragCatapultMovement.cs
@@ -10,20 +10,42 @@ public class DragCatapultMovement : MonoBehaviour {
 	[SerializeField]
 	private float speed = 500f;
 
+	private Plane plane;
+	private Ray ray;
+	private float distance = 20;
+	private Vector3 point;
+
+	private float maxDragDistance = 2f;
+
 	void Awake(){
+		plane = new Plane(Vector3.up, Vector3.zero);
 		myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+
 	}
 	
 	void OnMouseDown() 
 	{
-		mouseDownPos = Input.mousePosition;
+		
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(plane.Raycast(ray, out distance)) {
+			point = ray.GetPoint(distance);
+			if(Vector2.Distance(point, transform.position) <= maxDragDistance){
+				mouseDownPos = Input.mousePosition;
+				// Container.instance.DragStart();
+			}
+		}
 	}
 	
 	void OnMouseUp() 
 	{
-		mouseUpPos = Input.mousePosition;
-		var direction = mouseDownPos - mouseUpPos;
-		direction.Normalize();
-		myRigidbody.AddForce (direction * speed);
+		if(mouseDownPos != Vector2.zero){
+			mouseUpPos = Input.mousePosition;
+			var direction = mouseDownPos - mouseUpPos;
+			direction.Normalize();
+			myRigidbody.AddForce (direction * speed);
+		}
+
+		mouseDownPos = Vector2.zero;
+		// Container.instance.DragRelease();
 	}
 }
---
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/HakkemDoorDeMidden.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HakkemDoorDeMidden : MonoBehaviour {
+
+	[SerializeField]
+	private LayerMask enemyLayers;
+
+	[SerializeField]
+	private float areaOfAttack;
+
+	Collider2D[] enemiesToBeHakkedDoorDeMidden;
+
+	void LateUpdate () {
+		enemiesToBeHakkedDoorDeMidden = Physics2D.OverlapCircleAll(transform.position, areaOfAttack, enemyLayers,0,99);
+		foreach(Collider2D enemy in enemiesToBeHakkedDoorDeMidden){
+			
+		}
+	}
+}
