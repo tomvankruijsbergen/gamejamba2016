5572c018a99748c9cd6c63bf074c31b70942da31
Tom van Kruijsbergen <tom.v.kruijsbergen@gmail.com>
2016-11-19 14:55:04 +0100

    Added zoom distance.

--- GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/CameraBehaviour.cs
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/CameraBehaviour.cs
@@ -9,6 +9,14 @@ public class CameraBehaviour : MonoBehaviour {
 	[SerializeField]
 	private float snapDistance = 0.1f;
 
+	[SerializeField]
+	private float zoomBase = 9;
+	[SerializeField]
+	private float zoomPerDistance = 10;
+
+	[SerializeField]
+	new private Camera camera;
+
 	// Use this for initialization
 	void Awake () {
 		Container.instance.AssignCamera(transform);
@@ -26,11 +34,12 @@ public class CameraBehaviour : MonoBehaviour {
 	void OnPlayerMoved(Vector2 playerPosition) {
 		// Interpolate to the player.
 
-		float speedPerTime = speed * Time.deltaTime;
+		float speedPerTime = this.speed * Time.deltaTime;
 
+		Vector2 position = new Vector2(transform.position.x, transform.position.y);
 		Vector2 difference = new Vector2(
-			playerPosition.x - transform.position.x, 
-			playerPosition.y - transform.position.y
+			playerPosition.x - position.x, 
+			playerPosition.y - position.y
 		);
 
 		// Don't do anything if we're on the object.
@@ -44,10 +53,13 @@ public class CameraBehaviour : MonoBehaviour {
 			newPosition = playerPosition;
 		} else {
 			newPosition = new Vector2(
-				transform.position.x + speedPerTime * difference.x,
-				transform.position.y + speedPerTime * difference.y
+				position.x + speedPerTime * difference.x,
+				position.y + speedPerTime * difference.y
 			);
 		}
+		
+		float movedDistance = Vector2.Distance(newPosition, position);
+		this.camera.orthographicSize = zoomBase + movedDistance * zoomPerDistance;
 
 		transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
 	}
