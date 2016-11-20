07ca924667e09819c8c4c63569ca593f6a0da095
Luigi van der Pal <eentweedrie@gmail.com>
2016-11-20 03:09:54 +0100

    patrolling beta

---
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/EnemyPatrol.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyPatrol : MonoBehaviour {
+
+	[SerializeField] private Transform patrolPointA;
+	[SerializeField] private Transform patrolPointB;
+	[SerializeField] private float hopSpeed = 8f;
+	[SerializeField] private float patrolDuration = 5f;
+	[SerializeField] private float maxHopHeight = .5f;
+
+	private float baseY;
+	private float startTime;
+
+	void Awake() {
+		startTime = Time.time;
+		baseY = transform.position.y;
+		if(patrolPointA.position.y != patrolPointB.position.y) {
+			Debug.Log("Is the enemy really walking on a slope faggot???");
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = Time.time - startTime;
+		//value for going to B
+		float distancePercentage = (deltaTime % patrolDuration) / patrolDuration;
+		
+		//Are we going to A ???
+		Debug.Log(deltaTime % patrolDuration * 2f);
+		if(deltaTime % patrolDuration * 2f < patrolDuration) {
+			//morph value for to A
+			distancePercentage = 1f - distancePercentage;
+		}
+		distancePercentage = Mathf.Pow(distancePercentage, 3f);
+		// do the actual X positioning
+		Vector3 newPosition = Vector3.Lerp(patrolPointA.position, patrolPointB.position, distancePercentage);
+		if(newPosition.x > transform.position.x) {
+			transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
+		} else {
+			transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+		}
+		transform.position = newPosition;
+
+		//now do the y
+		transform.position = new Vector3(transform.position.x, baseY + Mathf.Abs(Mathf.Sin(deltaTime * hopSpeed)) *  maxHopHeight);
+	}
+}
