75b4a66770cce791f237caafcc5cd8906b2df561
Luigi van der Pal <eentweedrie@gmail.com>
2016-11-20 21:57:21 +0100

    WIP swords, en iets met armored

---
+++ GameJamba2016/Assets/GAMEJAMBA_OUR_ASSETS_HERE/Scripts/SwordControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySwordControl : MonoBehaviour {
+
+	[SerializeField] private GameObject klik;
+	[SerializeField] private GameObject hak;
+	[SerializeField] private float delay = .2f;
+	[SerializeField] private bool hideKlik = true;
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+
+	void OnTriggerStay2D(Collider2D other) {
+
+	}
+	
+	public void DoIt() {
+
+	}
+}
